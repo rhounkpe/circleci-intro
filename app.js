4f550dad851ae27e28f3d51f2be082893412def5
Rufin Hounkpe <rufin.hounkpe@lampiris.be>
2020-11-06 18:34:09 +0100

    Add App.js and dependencies

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res) {
+    res.send('Hello!!!');
+});
+
+app.listen(8083);
+module.exports.getApp = app;
